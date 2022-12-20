fbab46b5401470f6eadc1440feae8faa6d917ef8
Ariel <117614710+RielDreams@users.noreply.github.com>
2022-12-19 21:59:27 -0500

    adding styles to nav bar

--- server.js
+++ server.js
@@ -1,63 +1,61 @@
 ////////////////////
 //PACKAGES
 ////////////////////
-const express = require('express')
-const mongoose = require('mongoose')
-const methodOverride = require('method-override')
-require('dotenv').config()
-
+const express = require("express");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+require("dotenv").config();
 
 ////////////////////
 //intializing
 ////////////////////
-const server = express()
+const server = express();
 
 ////////////////////
 //DATA
 ////////////////////
-const meat = require('./models/meats')
-const cheese = require('./models/cheese')
-const addOns = require('./models/addOns')
-const { urlencoded } = require('express')
+const meat = require("./models/meats");
+const cheese = require("./models/cheese");
+const addOns = require("./models/addOns");
+const { urlencoded } = require("express");
 
 ////////////////////
 //CONFIG
 ////////////////////
-const PORT = process.env.PORT
-const DATABASE_URI = process.env.DATABASE_URI
+const PORT = process.env.PORT;
+const DATABASE_URI = process.env.DATABASE_URI;
 
 ////////////////////
 //MIDWARE
 ////////////////////
-server.use(express.static('./public'))
-server.use(methodOverride('_method'))
-server.use(urlencoded({extended: true}))
-mongoose.connect(DATABASE_URI)
-const db = mongoose.connection
+server.use(express.static("./public"));
+server.use(methodOverride("_method"));
+server.use(urlencoded({ extended: true }));
+mongoose.connect(DATABASE_URI);
+const db = mongoose.connection;
 
 ////////////////////
 //SEED/CREATE
 ////////////////////
-server.get('/createm', (req,res)=>{
-    meat.create(req.body, (err)=>{
-        console.log(meat)
-    })
-})
+server.get("/createm", (req, res) => {
+  meat.create(req.body, (err) => {
+    console.log(meat);
+  });
+});
 
-server.get('/createc', (req,res)=>{
-    cheese.create(req.body, (err, c)=>{
-        res.send(c)
-    })
-})
+server.get("/createc", (req, res) => {
+  cheese.create(req.body, (err, c) => {
+    res.send(c);
+  });
+});
 
-server.get('/createa', (req,res)=>{
-    addOns.create(req.body, (err)=>{
-        addOns.find({}, (err,find)=>{
-            res.send(find)
-        })
-    })
-})
-
+server.get("/createa", (req, res) => {
+  addOns.create(req.body, (err) => {
+    addOns.find({}, (err, find) => {
+      res.send(find);
+    });
+  });
+});
 
 ////////////////////
 //ROUTES
@@ -65,57 +63,57 @@ server.get('/createa', (req,res)=>{
 
 ////////////////////
 //INDEX
-server.get('/', (req,res)=>{
-    res.send('this is the home page')
-})
+server.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
-server.get('/orders', (req,res)=> {
-    res.send('this suppose to be a orders/cart page')
-})
+server.get("/contact", (req, res) => {
+  res.send("this is the contact page");
+});
+
+server.get("/orders", (req, res) => {
+  res.send("this suppose to be a orders/cart page");
+});
 
 ////////////////////
 //NEW
-server.get('/orders/new', (req, res)=>{
-    res.send('this is the new orders page')
-})
-
+server.get("/orders/new", (req, res) => {
+  res.send("this is the new orders page");
+});
 
 ////////////////////
 //DESTORY
-server.delete('/orders/:id', (req,res)=> {
-    console.log('this is the cancel order page')
-})
-
+server.delete("/orders/:id", (req, res) => {
+  console.log("this is the cancel order page");
+});
 
 ////////////////////
 //UPDATE
-server.put('orders/:id', (req,res)=>{
-    res.send('this is the updated page')
-})
+server.put("orders/:id", (req, res) => {
+  res.send("this is the updated page");
+});
 
 ////////////////////
 //CREATE
-server.post('/orders', (req,res)=>{
-    console.log('this is creating orders')
-})
+server.post("/orders", (req, res) => {
+  console.log("this is creating orders");
+});
 
 ////////////////////
 //EDIT
-server.use('/orders/:id/edit', (req,res)=>{
-    console.log('editting orders')
-})
-
+server.use("/orders/:id/edit", (req, res) => {
+  console.log("editting orders");
+});
 
 ////////////////////
-//SHOW 
-server.get('/:id', (req, res)=>{
-    res.send('this is the showing page')
-})
-
+//SHOW
+server.get("/:id", (req, res) => {
+  res.send("this is the showing page");
+});
 
 ////////////////////
 //LISTENING
 ////////////////////
 server.listen(PORT, () => {
-    console.log(`BloomBoards is now online at ${PORT}`)
-})
+  console.log(`BloomBoards is now online at ${PORT}`);
+});
